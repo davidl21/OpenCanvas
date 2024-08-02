fc307fbe6cf059d4c6fc06be6fb40358b1d195d0
David Liu <davidliu2117@gmail.com>
2024-08-02 00:44:20 -0700

    testing changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl underline font-medium">Hello,</h1>
+      <h1 className="text-3xl underline font-medium">Hello, It's Me</h1>
     </div>
   );
 };
