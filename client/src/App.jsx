c1fd89acb0a6d8c71b245f4b84fe9d92b70261c3
David Liu <davidliu2117@gmail.com>
2024-08-02 00:43:55 -0700

    testing change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl underline font-medium">
-        Hello, this shoud be Rubik but yeah
-      </h1>
+      <h1 className="text-3xl underline font-medium">Hello,</h1>
     </div>
   );
 };
