5020132f9b1073d2c859b72efedc2ad086a92e78
David Liu <davidliu2117@gmail.com>
2024-08-02 00:41:36 -0700

    added rubik font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h1 className="text-3xl underline font-medium">
+        Hello, this shoud be Rubik but yeah
+      </h1>
     </div>
   );
 };
