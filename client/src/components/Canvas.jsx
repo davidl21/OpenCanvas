e1286c80323e104573563525c03a41bc19b7292f
David Liu <davidliu2117@gmail.com>
2024-08-03 04:50:24 -0700

    implemented snap to grid for pixels

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -6,6 +6,8 @@ const Canvas = () => {
   const canvasRef = useRef(null);
   // default black
   const [color, setColor] = useState({ r: 0, g: 0, b: 0 });
+  const gridSize = 1000;
+  const pixelSize = 5;
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -15,14 +17,24 @@ const Canvas = () => {
       console.log(color);
       context.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
       console.log(`rgb(${color.r}, ${color.g}, ${color.b})`);
-      context.fillRect(x, y, 5, 5);
+      context.fillRect(x, y, pixelSize, pixelSize);
     };
 
     const handleMouseDown = (event) => {
       const rect = canvas.getBoundingClientRect();
       const x = event.clientX - rect.left;
       const y = event.clientY - rect.top;
-      draw(x, y);
+      console.log(x, y);
+      const gridX = Math.floor(x / gridSize) * gridSize;
+      const gridY = Math.floor(y / gridSize) * gridSize;
+
+      // Snap to 5x5 grid within the 1000x1000 grid
+      const snappedX =
+        Math.floor((x % gridSize) / pixelSize) * pixelSize + gridX;
+      const snappedY =
+        Math.floor((y % gridSize) / pixelSize) * pixelSize + gridY;
+      console.log(gridX, gridY);
+      draw(snappedX, snappedY);
     };
 
     canvas.addEventListener("mousedown", handleMouseDown);
@@ -36,8 +48,8 @@ const Canvas = () => {
     <div className="mt-10 flex flex-col justify-center items-center">
       <canvas
         ref={canvasRef}
-        width={1000}
-        height={1000}
+        width={gridSize}
+        height={gridSize}
         className="border border-orange-300 rounded-3xl"
       />
       <ColorPicker onColorChange={setColor} />
